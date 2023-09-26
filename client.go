350feedeb90aa5ba13b861a38ede3ff0b26635ae
ionling <soarvision@outlook.com>
2023-09-26 10:29:35 +0800

    feat(paypal): support `Auth` method
    - feat(paypal): add Error struct

---
+++ client.go
@@ -0,0 +1,176 @@
+package paypal
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Client struct {
+	base, id, secret string
+
+	t  *Token
+	hc *http.Client
+}
+
+func New(base, id, secret string) *Client {
+	return &Client{
+		base:   base,
+		id:     id,
+		secret: secret,
+		hc:     &http.Client{},
+	}
+}
+
+type Token struct {
+	Scope       string `json:"scope"`
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	AppID       string `json:"app_id"`
+	Nonce       string `json:"nonce"`
+	ExpiresIn   int    `json:"expires_in"`
+	expiresAt   time.Time
+}
+
+func (t *Token) Valid() bool {
+	if t == nil {
+		return false
+	}
+	return t.expiresAt.After(time.Now())
+}
+
+// Auth requests a new token from PayPal server.
+// See https://developer.paypal.com/api/rest/authentication/.
+func (c *Client) Auth(ctx context.Context) (res *Token, err error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost,
+		c.base+"/v1/oauth2/token", strings.NewReader("grant_type=client_credentials"))
+	if err != nil {
+		return nil, fmt.Errorf("new request: %w", err)
+	}
+	req.SetBasicAuth(c.id, c.secret)
+
+	start := time.Now()
+	if res, err = doJSON[Token](ctx, c, req); err != nil {
+		return
+	}
+
+	// Minus 2 seconds is to prevent expiration due to network latency.
+	res.expiresAt = start.Add(time.Duration(res.ExpiresIn-2) * time.Second)
+	return
+}
+
+func (c *Client) checkToken(ctx context.Context) (err error) {
+	if !c.t.Valid() {
+		c.t, err = c.Auth(ctx)
+	}
+	return
+}
+
+// JSON performs the request with the data marshaled to JSON format,
+// unmarshals the response body into a new R,
+// and automatically refreshes the client's access token.
+func JSON[R any](ctx context.Context, c *Client, method, path string, data any,
+) (res *R, err error) {
+	if err = c.checkToken(ctx); err != nil {
+		return
+	}
+	url := c.base + path
+	req, err := NewJSONRequest(ctx, method, url, data)
+	if err != nil {
+		return nil, fmt.Errorf("new request: %w", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+c.t.AccessToken)
+	return doJSON[R](ctx, c, req)
+}
+
+func doJSON[R any](ctx context.Context, c *Client, req *http.Request) (res *R, err error) {
+	hres, err := c.hc.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("do: %w", err)
+	}
+
+	if hres.StatusCode < 400 {
+		return RespJSON[R](hres)
+	}
+	e, err := RespJSON[Error](hres)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal error: %w", err)
+	}
+	e.StatusCode = hres.StatusCode
+	return nil, e
+}
+
+// Error is the PayPal API error response.
+// See https://developer.paypal.com/api/rest/responses/.
+type Error struct {
+	StatusCode int
+
+	Name    string         `json:"name"`
+	Message string         `json:"message"`
+	DebugID string         `json:"debug_id"`
+	Details []*ErrorDetail `json:"details"`
+	Links   []*Link        `json:"links"`
+
+	// For identity errors
+	Err     string `json:"error"`
+	ErrDesc string `json:"error_description"`
+}
+
+func (e *Error) Error() string {
+	if e.Err != "" {
+		return e.Err + ": " + e.ErrDesc
+	}
+	return fmt.Sprintf("%s: %s (%s)", e.Name, e.Message, e.DebugID)
+}
+
+type ErrorDetail struct {
+	Field       string `json:"field"`
+	Value       string `json:"value"`
+	Location    string `json:"location"`
+	Issue       string `json:"issue"`
+	Description string `json:"description"`
+}
+
+// Link is a HATEOAS link.
+// See https://developer.paypal.com/api/rest/responses/#link-hateoaslinks.
+type Link struct {
+	HRef   string `json:"href"`
+	Rel    string `json:"rel"`
+	Method string `json:"method"`
+}
+
+// NewJSONRequest returns a new [http.Request] with the given data marshaled to JSON format.
+func NewJSONRequest(ctx context.Context, method, url string, data any,
+) (res *http.Request, err error) {
+	var r io.Reader
+	if data != nil {
+		bs, err := json.Marshal(data)
+		if err != nil {
+			return nil, err
+		}
+		r = bytes.NewReader(bs)
+	}
+	res, err = http.NewRequestWithContext(ctx, method, url, r)
+	if err != nil {
+		return
+	}
+	res.Header.Set("Content-Type", "application/json")
+	return
+}
+
+// RespJSON unmarshals the response body into a new R and closes the body afterwards.
+func RespJSON[R any](r *http.Response) (res *R, err error) {
+	defer r.Body.Close()
+	bs, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	res = new(R)
+	err = json.Unmarshal(bs, res)
+	return
+}
---
+++ client_test.go
@@ -0,0 +1,82 @@
+package paypal
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/adobaai/paypal/ptesting"
+)
+
+func NewTestClient() *Client {
+}
+
+func TestAuth(t *testing.T) {
+	ctx := context.Background()
+	c := NewTestClient()
+	token := ptesting.R(c.Auth(ctx)).NoError(t).V()
+	assert.NotZero(t, token.Scope)
+	assert.NotZero(t, token.AccessToken)
+	assert.Equal(t, token.TokenType, "Bearer")
+	assert.NotZero(t, token.AppID)
+	assert.NotZero(t, token.ExpiresIn)
+	assert.NotZero(t, token.Nonce)
+}
+
+func TestError(t *testing.T) {
+	ctx := context.Background()
+	t.Run("Auth", func(t *testing.T) {
+		c := New("https://api-m.sandbox.paypal.com", "1234", "5678")
+		ptesting.R(c.Auth(ctx)).EqualError(t, "invalid_client: Client Authentication failed")
+	})
+
+	t.Run("CreateOrder", func(t *testing.T) {
+		c := NewTestClient()
+		var e *Error
+		ptesting.R(c.CreateOrder(ctx, &CreateOrderReq{Intent: OICapture})).ErrorAs(t, &e)
+		assert.NotZero(t, e.DebugID)
+		e.DebugID = ""
+		assert.Equal(t, &Error{
+			StatusCode: http.StatusBadRequest,
+			Name:       "INVALID_REQUEST",
+			Message:    "Request is not well-formed, syntactically incorrect, or violates schema.",
+			Details: []*ErrorDetail{
+				{
+					Field:       "/purchase_units",
+					Location:    "body",
+					Issue:       "MISSING_REQUIRED_PARAMETER",
+					Description: "A required field / parameter is missing.",
+				},
+			},
+			Links: []*Link{
+				{
+					HRef:   "https://developer.paypal.com/docs/api/orders/v2/#error-MISSING_REQUIRED_PARAMETER",
+					Rel:    "information_link",
+					Method: "",
+				},
+			},
+		}, e)
+	})
+}
+
+type Hello struct {
+	Name string
+}
+
+type HelloResp struct {
+	JSON *Hello
+}
+
+func TestJSON(t *testing.T) {
+	c := &http.Client{}
+	ctx := context.Background()
+	name := "Фёдор Миха́йлович Достое́вский"
+	url := "https://httpbin.org/anything"
+	hreq := ptesting.R(NewJSONRequest(ctx, http.MethodPost, url, Hello{Name: name})).NoError(t).V()
+	hres := ptesting.R(c.Do(hreq)).NoError(t).V()
+	ptesting.R(RespJSON[HelloResp](hres)).NoError(t).Do(func(t *testing.T, it *HelloResp) {
+		assert.Equal(t, name, it.JSON.Name)
+	})
+}
